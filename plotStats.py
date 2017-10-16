974b3b306a8cf2659eac8237ed3dd107dba7724b
enriquerod <luis_e@outlook.com>
2017-10-16 10:51:24 -0500

    modified script to plot pie graph

--- plotStats.py
+++ plotStats.py
@@ -1,48 +1,72 @@
+from unet import *
+# from data import *
 import matplotlib.pyplot as plt
-import numpy as np
+from keras.models import load_model
+import cv2
 
-from scipy.io import loadmat
+# data = loadmat("dataset.mat")
+# imgs_train = data["train"]
+# img = imgs_train[0]
+# plt.imshow(img, cmap='gray')
+# plt.show()
 
-path_history =  "C:/Users/EHO085/Desktop/unet-master2/weights-val-gray-256/history.csv"
 
-seg_history = np.genfromtxt(path_history, delimiter=",", dtype=None)
-epoch = seg_history[1:,0]
-acc = seg_history[1:,1]
-val_acc = seg_history[1:,3]
-loss = seg_history[1:,2]
-val_loss = seg_history[1:,4]
+# img = cv2.imread("C:/Users/EHO085/Desktop/data_skin/ISIC-2017_Validation_Data/ISIC_0012684.jpg")
+# img = cv2.imread("C:/Users/EHO085/Desktop/data_skin/ISIC-2017_Validation_Data/ISIC_0001871.jpg")
+img = cv2.imread("C:/Users/EHO085/Desktop/data_skin/ISIC-2017_Validation_Data/ISIC_0009995.jpg")
 
-# seg_history = seg_history.decode("utf-8")
-
-# fl = path_data + '/' + files.decode("utf-8")  + '.jpg'
-# X1 = get_im(fl, img_w, img_h, img_d)
-
-plt.plot(epoch, acc, label='Acc')
-plt.plot(epoch, val_acc, label='Val_acc') 
-plt.plot(epoch, loss, label='Loss')
-plt.plot(epoch, val_loss, label='Val_loss') 
-plt.xlabel('Epochs')
-# plt.ylabel('y label')
-
-plt.title("Segmentation Metrics")
-
-plt.legend()
-# plt.plot([1,2,3,4], [1,4,9,16], 'ro')
-# plt.axis([0, 6, 0, 20])
+img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+img = cv2.resize(img, (256,256))
+# imgp = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+plt.imshow(img, cmap='gray')
 plt.show()
 
 
-path_histo = "C:/Users/EHO085/Desktop/unet-master2/data/ISIC-2017_Training_Part3_GroundTruth.csv"
-data_histo = np.genfromtxt(path_histo, delimiter=",", dtype=None)
-mela = data_histo[1:,1]
+# mydata = dataProcess(512,512)
 
-mela = list(map(float,mela))
-mela = list(map(int, mela))
+# imgs_test = mydata.load_test_data()
 
-hist = plt.hist(mela, bins=np.arange(0, 3, 1), color=['b'])
-# hist = plt.hist(mela, normed=True, bins='auto')
-x = [0.5, 1.5]
-plt.xticks(x, ['Benign','Malignant'])
-plt.title("ISIC 2017 Melanoma Histogram")
-# plt.xlabel("Melanoma")
+# imgs_test = tiff.imread('data/test-volume.tif')
+# # imgs_test = tiff.imread('data/train-volume.tif')
+# imgs_test = np.array(imgs_test)
+# print("AQUI",imgs_test.shape)
+# print(imgs_test[0])
+# plt.imshow(imgs_test[2], cmap='gray')
+# plt.show()
+# imgs_test = imgs_test.reshape(imgs_test.shape[0], 512, 512, 1)
+# imgs_test = imgs_test.astype('float32')
+# imgs_test /= 255
+# mean = imgs_test.mean(axis = 0)
+# imgs_test -= mean	
+# print(imgs_test.shape)
+# img = imgs_test[2]
+img = img.reshape(1, 256, 256, 1)
+print(img.shape)
+
+# model = load_model('unet.hdf5', compile=False)
+myunet = myUnet()
+
+model = myunet.get_unet()
+
+# model.load_weights('weights-00.hdf5')
+# model.load_weights('weights-18.hdf5')
+# model.load_weights('weights-val-gray-256-aug/weights-09.hdf5')
+model.load_weights('weights-val-gray-256-aug/weights-11.hdf5')
+
+imgs_mask_test = model.predict(img, verbose=1)
+print(imgs_mask_test[0])
+imgs_mask_test[imgs_mask_test > 0.5] = 255
+imgs_mask_test[imgs_mask_test <= 0.5] = 0
+
+# np.save('imgs_mask_test.npy', imgs_mask_test)
+
+print(imgs_mask_test.shape)
+
+img_pred = imgs_mask_test[0]
+img_pred = img_pred.reshape(256, 256)
+# img_pred = img_pred*255
+img_pred = img_pred.astype(np.uint8)
+print(img_pred)
+print("AQUI2",img_pred.shape)
+plt.imshow(img_pred, cmap='gray')
 plt.show()
